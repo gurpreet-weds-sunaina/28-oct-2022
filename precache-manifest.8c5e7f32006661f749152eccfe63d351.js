d3bf7ef080fce26ea16f8df39e32ece3de10e0ed
Gurpreet Singh <gurpreet.singh6@telus.com>
2022-10-20 14:13:28 +0530

    Updates

---
+++ precache-manifest.8c5e7f32006661f749152eccfe63d351.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a6099767c5d771e6c51c3ecc7f6bc6a",
+    "url": "/amrit-weds-mandeep/index.html"
+  },
+  {
+    "revision": "55a577cd4aa421ecbfdf",
+    "url": "/amrit-weds-mandeep/static/js/2.def50c6c.chunk.js"
+  },
+  {
+    "revision": "ee2968b2326bdba77ea2",
+    "url": "/amrit-weds-mandeep/static/js/main.2a74e12e.chunk.js"
+  },
+  {
+    "revision": "a206665a616d66da18d8",
+    "url": "/amrit-weds-mandeep/static/js/runtime-main.83f79547.js"
+  }
+]);
